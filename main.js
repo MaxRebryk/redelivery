0cc063fb9abce656c3197fd7bc71f549484c45a2
Max Rebryk <rebryk2003@gmail.com>
2024-03-16 13:46:27 +0300

    add overflow-hidden for mobile menu

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ const mobileMenuCloseBtn = document.querySelector(".close-mobile-menu-btn");
 function togleMobileMenu (){
     const mobileMenu = document.querySelector(".modal-mobile-menu");
     mobileMenu.classList.toggle("is-open");
+    document.body.style.overflow = "hidden"; 
 }
 
 mobileMenuOpenBtn.addEventListener("click", togleMobileMenu);
-mobileMenuCloseBtn.addEventListener("click",togleMobileMenu);
+mobileMenuCloseBtn.addEventListener("click",(event) =>{
+    togleMobileMenu();
+    document.body.style.overflow = "auto";
+});
