2ad91596af679114c5caeaa076d89e1eb0ce744f
Max Rebryk <rebryk2003@gmail.com>
2024-03-16 13:22:10 +0300

    start working at mobile menu

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+
+const mobileMenuOpenBtn = document.querySelector(".mobile-menu-open-button");
+const mobileMenuCloseBtn = document.querySelector(".close-mobile-menu-btn");
+
+
+function togleMobileMenu (){
+    const mobileMenu = document.querySelector(".modal-mobile-menu");
+    mobileMenu.classList.toggle("is-open");
+}
+
+mobileMenuOpenBtn.addEventListener("click", togleMobileMenu);
+mobileMenuCloseBtn.addEventListener("click",togleMobileMenu);
