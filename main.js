5fa3bd1e1f61faf1ddcd68e19047dcbfa67fcf74
Max Rebryk <rebryk2003@gmail.com>
2024-03-23 16:00:09 +0300

    Done look menu buttons functional

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 
 const mobileMenuOpenBtn = document.querySelector(".mobile-menu-open-button");
 const mobileMenuCloseBtn = document.querySelector(".close-mobile-menu-btn");
+const greetMenuBtn = document.querySelector(".greet-menu-btn");
+const recomMenyBtn = document.querySelector(".recom-menu-btn");
 
 
 function togleMobileMenu (){
@@ -11,8 +13,21 @@ function togleMobileMenu (){
     document.body.style.overflow = "hidden"; 
 }
 
+function hrefTo (url){
+    window.location.href = url;
+}
+
+
 mobileMenuOpenBtn.addEventListener("click", togleMobileMenu);
 mobileMenuCloseBtn.addEventListener("click",(event) =>{
     togleMobileMenu();
     document.body.style.overflow = "auto";
+});
+
+greetMenuBtn.addEventListener("click",(event) =>{
+    hrefTo("./src/html/menu.html");
+});
+
+recomMenyBtn.addEventListener("click",(event) =>{
+    hrefTo("./src/html/menu.html");
 });
