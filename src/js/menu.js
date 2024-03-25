0711337419d2bec4259ef4f1bbac49d95bb626f7
Max Rebryk <rebryk2003@gmail.com>
2024-03-25 18:15:23 +0300

    done search menu functional

--- src/js/menu.js
+++ src/js/menu.js
@@ -11,6 +11,7 @@ const menuHeader = document.querySelector(".menu-header");
 const menu = document.querySelector(".menu-list");
 const modalMenuCloseBtn = document.querySelector(".close-modal-menu-btn");
 const backdrop = document.querySelector(".backdrop");
+const searchInput = document.querySelector(".search-input");
 let menuAddItemButtons = document.querySelectorAll(".menu-list-item-add-button");
 let counter = 1;
 
@@ -257,6 +258,7 @@ function changeCategoryMenu(btn){
         const newMarkup = `<span class="menu-new">New</span>`
         menuListItemContainer.insertAdjacentHTML("afterbegin", newMarkup);
       }
+  
         })
 
     }
@@ -308,6 +310,7 @@ function changeCategoryMenu(btn){
         })
 
     }
+   
 }
 
 function getItemById(itemId) {
@@ -353,8 +356,83 @@ function displayItemModal(itemId) {
 
 modalContent.innerHTML="";
 modalContent.insertAdjacentHTML("afterbegin", markup);
-
 }
+
+function findItemByText(text) {
+  const textLowerCase = text.toLowerCase();
+  const matchingItems = [];
+  
+  pizza.forEach(item => {
+    if (item.name.toLowerCase().includes(textLowerCase)) {
+      matchingItems.push(item);
+    }
+  });
+
+  burgers.forEach(item => {
+    if (item.name.toLowerCase().includes(textLowerCase)) {
+      matchingItems.push(item);
+    }
+  });
+
+  pita.forEach(item => {
+    if (item.name.toLowerCase().includes(textLowerCase)) {
+      matchingItems.push(item);
+    }
+  });
+  return matchingItems;
+
+ 
+}
+
+function renderSearchResults(resultArray){
+   menu.innerHTML = "";
+      menuHeader.textContent = "Результат пошуку";
+      resultArray.forEach(item => {
+          const markup = `<li class="menu-list-item">
+      <div class="menu-list-item-container">
+        
+        <div class="svg-like-container">
+        <button class="menu-like-btn" type="button"><svg
+      class="like-icon"
+      aria-label="like-icon"
+      width="24"
+      height="24"
+      >
+      <use href="../svg/symbol-defs.svg#icon-heart-outline"></use>
+      </svg></button>
+    </div>
+      <picture>
+        <source
+          srcset="
+            ${item.imgDesktop} 1x,
+            ${item.imgDesktop2x} 2x
+          "
+          media="(min-width:1158px)"
+          height="300"
+          width="360"
+        />
+     
+        <source
+          srcset="${item.imgMobile2x} 2x"
+          media="(max-width: 767px)"
+        />
+        <img class="menu-img" src="${item.imgMobile}" alt="pizza">
+      </picture>
+      <h3 class="menu-list-item-header">${item.name}</h3>
+      <p class="menu-list-item-price">${item.price} грн</p>
+      <button class="menu-list-item-add-button" type="button" data-info="${item.id}">Додати</button>
+      </div>
+    </li>`
+    
+    menu.insertAdjacentHTML("beforeend", markup);
+    if(item.new){
+      const menuListItemContainer = document.querySelector(".menu-list-item-container");
+      const newMarkup = `<span class="menu-new">New</span>`
+      menuListItemContainer.insertAdjacentHTML("afterbegin", newMarkup);
+    }
+      })
+    };
+
 
 
 
@@ -418,4 +496,16 @@ countMinusBtn.addEventListener("click",(event) => {
     itemsCounter.innerHTML = counter;
   }
  
+})
+
+searchInput.addEventListener("input", (event) => {
+
+  event.preventDefault();
+  let text = event.target.value;
+  categoryMenuPizzaBtn.classList.remove("is-active");
+  categoryMenuBurgerBtn.classList.remove("is-active");
+  categoryMenuPitaBtn.classList.remove("is-active");
+  const matchingItems = findItemByText(text); // Отримання відповідних елементів
+  renderSearchResults(matchingItems);
+
 })
