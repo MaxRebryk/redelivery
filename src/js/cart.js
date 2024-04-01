006cd33e796d940079481688ef166f2fb8d31d57
Max Rebryk <rebryk2003@gmail.com>
2024-04-01 07:22:13 +0300

    Update cart.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,7 +1,7 @@
 "use strict";
 
-import * as mobileMenu from './mobile-menu.js';
-import { pizza, burgers, pita } from './menu-list.js';
+import * as mobileMenu from '../js/mobile-menu.js';
+import { pizza, burgers, pita } from '../js/menu-list.js';
 
 
 
